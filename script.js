75ffa85c4b1b8c500aaa578a373c09cdb8368fd1
shreyasaini003 <103727732+shreyasaini003@users.noreply.github.com>
2024-04-10 23:16:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const wraaper = document.querySelector(".wraaper");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
+const btnPopup = document.querySelector(".btnLogin-popup");
+
+registerLink.addEventListener('click',()=> {
+    wraaper.classList.add('active');
+
+});
+
+loginLink.addEventListener('click',()=> {
+    wraaper.classList.remove('active');
+
+});
+btnPopup.addEventListener('click',()=> {
+    wraaper.classList.add('active-popup');
+
+});
